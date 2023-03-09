2f9684edd158e95e01256c5c7faad173cf937ea2
Leila Abache <leila.aba@hotmail.fr>
2023-03-09 09:09:47 +0100

    correction avant envoi

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -28,44 +28,48 @@ const urlLogin = urlBase + "/users/login";
 document
   .querySelector(".form")
   .addEventListener("submit", async function (event) {
-    // J'annule le comportement par défaut du navigateur (chargement d'une nouvelle page à la validation du formulaire)
-    event.preventDefault();
+    try {
+      // J'annule le comportement par défaut du navigateur (chargement d'une nouvelle page à la validation du formulaire)
+      event.preventDefault();
 
-    // Je récupère les données eventuellement stockées dans le localStorage
-    let dataResponse = window.localStorage.getItem("dataResponse");
+      // Je récupère les données eventuellement stockées dans le localStorage
+      let dataResponse = window.localStorage.getItem("dataResponse");
 
-    let messageErreurLogin = document.querySelector(".erreur-login");
+      let messageErreurLogin = document.querySelector(".erreur-login");
 
-    // Si aucune donnée sauvegardée dans le localStorage
-    if (dataResponse === null) {
-      // J'envoie les données de connexion et demande à récupérer une reponse via fetch
-      const response = await fetch(urlLogin, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: document.getElementById("email").value,
-          password: document.getElementById("mot-de-passe").value,
-        }),
-      });
-      dataResponse = await response.json();
+      // Si aucune donnée sauvegardée dans le localStorage
+      if (dataResponse === null) {
+        // J'envoie les données de connexion et demande à récupérer une reponse via fetch
+        const response = await fetch(urlLogin, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: document.getElementById("email").value,
+            password: document.getElementById("mot-de-passe").value,
+          }),
+        });
+        dataResponse = await response.json();
 
-      // Je crée une condition pour rediriger l'utilisateur sur la page home en cas de connexion
-      if ("token" in dataResponse) {
-        document.location.href = "index.html";
+        // Je crée une condition pour rediriger l'utilisateur sur la page home en cas de connexion
+        if ("token" in dataResponse) {
+          document.location.href = "index.html";
 
-        //Je convertis dataResponse en JSON avant de les enregistrer dans localStorage
-        const valeurData = JSON.stringify(dataResponse);
-        // Stockage des informations dans le localStorage
-        window.localStorage.setItem("dataResponse", valeurData);
+          //Je convertis dataResponse en JSON avant de les enregistrer dans localStorage
+          const valeurData = JSON.stringify(dataResponse);
+          // Stockage des informations dans le localStorage
+          window.localStorage.setItem("dataResponse", valeurData);
+        } else {
+          // Si erreur connexion, je renvoie un msg d'erreur
+          messageErreurLogin.style.visibility = "visible";
+        }
       } else {
-        // Si erreur connexion, je renvoie un msg d'erreur
-        messageErreurLogin.style.visibility = "visible";
+        //Si les données sont déjà sauvegardées, je les reconstruis en mémoire, afin de rester connecté tant que pas déloguée
+        dataResponse = JSON.parse(dataResponse);
       }
-    } else {
-      //Si les données sont déjà sauvegardées, je les reconstruis en mémoire, afin de rester connecté tant que pas déloguée
-      dataResponse = JSON.parse(dataResponse);
+    } catch (error) {
+      console.error(error);
     }
   });
 
