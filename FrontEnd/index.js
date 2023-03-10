a84e8abdafbd0960bbcf6f850b9cf0835c54e1d5
Leila Abache <leila.aba@hotmail.fr>
2023-03-10 10:24:45 +0100

    mise à jour definitive

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,29 +1,26 @@
-class Work {
-  constructor(jsonWork) {
-    jsonWork && Object.assign(this, jsonWork);
-  }
-}
-
 //-----------------------------------------------------------------------------------------------//
-//----------- Appel fetch à l'API pour récupérer les travaux (works) et les categories ----------//
+//----------- Appel fetch à l'API pour récupérer les travaux (works) et les catégories ----------//
 
-// Je crée une variable avec le lien général de l'api;
+// Variable avec le lien général de l'api;
 const urlBase = "http://localhost:5678/api";
 
-// Je crée une variable qui stockera l'appel des travaux de l'api via fetch
+// Variable qui stockera les travaux, une autre pour les catégories
 let jsonWorks = null;
+let jsonCategories = null;
+// Variable pour pointer la galerie html
+const galleryHtml = document.querySelector("#gallery");
 
+// Fonction pour récupérer les travaux (works) via fetch
 const getWorks = async function () {
   try {
-    jsonWorks = await (await fetch(`${urlBase}/works`)).json();
-    return jsonWorks;
+    return await (await fetch(`${urlBase}/works`)).json();
   } catch (error) {
-    console.error(
-      "Une erreur est survenue lors de la récupération des projets :",
-      error
-    );
+    alert("Une erreur est survenue, veuillez réessayer ultérieurement", error);
   }
 };
+// Récupère le tableau des works en les stockant dans la variable jsonWorks
+jsonWorks = await getWorks();
+
 // const getWorks = function() {
 // return fetch("http://localhost:5678/api/works")
 // .then((response) => response.json())
@@ -35,60 +32,60 @@ const getWorks = async function () {
 // });
 // };
 
-// Je crée une variable pour pointer la galerie html
-const galleryHtml = document.querySelector("#gallery");
+// Fonction pour récupérer le tableau Catégories via fetch
+const getCategories = async function () {
+  try {
+    return await (await fetch(`${urlBase}/categories`)).json();
+  } catch (error) {
+    alert("Une erreur est survenue", error);
+  }
+};
+// Récupère le tableau des catégories en les stockant dans la variable jsonCatégories
+jsonCategories = await getCategories();
 
-// Je récupère le tableau le tableau works de l'API
-jsonWorks = await getWorks();
+//------------------------------------------------------------------------------------------//
+//-------------------- Création et affichage de la galerie des projets ---------------------//
 
-// Je récupère le tableau Catégories de l'API
-const jsonCategories = await (await fetch(`${urlBase}/categories`)).json();
-
-// Je crée une fonction globale genererWorks pour la mise à jour des travaux dans la galerie
+// Fonction globale genererWorks pour la mise à jour des travaux dans la galerie
 const genererWorks = function (jsonWorks) {
-  // Je régénère la page avec un body vide pour ensuite afficher correctement la mise à jour
+  // Régénère la page avec un body vide pour ensuite afficher correctement la mise à jour
   galleryHtml.innerHTML = "";
 
-  //------------------------------------------------------------------------------------------//
-  //-------------------- Création et affichage de la galerie des projets ---------------------//
-
-  // Je parcours le tableau des works, je crée les balises html et j'affiche les éléments du tableau;
+  // Parcours le tableau des works, crée les balises html et affiche les éléments du tableau;
   for (let jsonWork of jsonWorks) {
-    let work = new Work(jsonWork);
-
     galleryHtml.innerHTML += `<figure>
-      <img src=${work.imageUrl}>
-      <figcaption>${work.title}</figcaption>
+      <img src=${jsonWork.imageUrl}>
+      <figcaption>${jsonWork.title}</figcaption>
       </figure>`;
   }
 };
 genererWorks(jsonWorks);
 
-//-----------------------------------------------------------------------//
-//----------------- Création et affichage des Filtres -------------------//
+//-----------------------------------------------------------------------------------------//
+//-------------------------- Création et affichage des Filtres ----------------------------//
 
-// Je crée un bouton pour afficher tous les projets;
+// Crée un bouton pour afficher tous les projets;
 const boutonTous = document.createElement("button");
 boutonTous.innerText = "Tous";
-// Je rattache le bouton à son parent HTML;
+// Rattache le bouton à son parent HTML;
 document.getElementById("filtre-categories").appendChild(boutonTous);
 
-// Je crée une boucle qui parcourt le tableau des catégories;
+// Boucle qui parcourt le tableau des catégories;
 for (let i = 0; i < jsonCategories.length; i++) {
   const elFiltreCategories = document.getElementById("filtre-categories");
 
   const categorie = jsonCategories[i];
 
-  // Je crée les boutons filtre en fonction de leur nom;
+  // Crée les boutons filtre en fonction de leur nom;
   const boutonFiltre = document.createElement("button");
-  // Je leur ajoute le nom de la categorie du backend ainsi que son id
+  // Ajoute le nom de la categorie du backend ainsi que son id
   boutonFiltre.innerText = categorie.name;
   boutonFiltre.dataset.catid = categorie.id;
-  // Je rattache les boutons filtre à leur parent HTML;
+  // Rattache les boutons filtre à leur parent HTML;
   elFiltreCategories.appendChild(boutonFiltre);
 }
 
-// Je crée un événement au click pour filtrer les works en fonction de leur catégorie
+// Événement au click pour filtrer les works en fonction de leur catégorie
 let boutonsFiltre = document.querySelectorAll("#filtre-categories button");
 for (let boutonFiltre of boutonsFiltre) {
   boutonFiltre.addEventListener("click", function () {
@@ -98,17 +95,18 @@ for (let boutonFiltre of boutonsFiltre) {
         // Si bouton TOUS
         return true; // Garde ce work
       } else if (workCat.category.id == thisButton.dataset.catid) {
-        // Si autre bouton : je garde si la cat du Works correspond à la cat du bouton
+        // Si autre bouton : Garde si la cat du Works correspond à la cat du bouton
         return true; // Garde ce work
       }
     });
-    // Je fais appel à la fonction qui régénère les works (en vidant d'abord le contenu de la galerie pour ensuite n'afficher que les works liés au click)
+    // Appel à la fonction qui régénère les works (en vidant d'abord le contenu de la galerie pour ensuite n'afficher que les works liés au click)
     genererWorks(worksFiltres);
   });
 }
 
+//---------------------------------------------------------------------------------//
 //----------------------------- Les données du localstorage -----------------------//
-// Je crée un évenement pour effacer les données du localStorage à la deconnexion et redirection vers index
+// Évenement pour effacer les données du localStorage à la deconnexion et redirection vers index
 const boutonLogout = document.querySelector(".logout");
 boutonLogout.addEventListener("click", function (event) {
   event.preventDefault();
@@ -116,13 +114,13 @@ boutonLogout.addEventListener("click", function (event) {
   document.location.href = "index.html";
 });
 
-//Je récupère les data je les parse pour changer la config html de l'index si connecté ou non
+// Récupère les data et les parse pour changer la config html de l'index si connecté ou non
 let dataResponse = window.localStorage.getItem("dataResponse");
 dataResponse = JSON.parse(dataResponse);
 
-//--------------------------------------------------------------------------//
-//-------------------- Apparition du mode edition --------------------------//
-// Je fais apparaitre les éléments cachés si logged
+//----------------------------------------------------------------------------------//
+//---------------------------- Apparition du mode edition --------------------------//
+// Fait apparaitre les éléments cachés si logged
 let homeEditLogin = document.getElementsByClassName("log");
 
 for (let element of homeEditLogin) {
@@ -133,7 +131,7 @@ for (let element of homeEditLogin) {
   }
 }
 
-// Je cache le lien "login" si logged
+// Cache le lien "login" si logged
 let homeEditLogout = document.getElementsByClassName("login");
 
 for (let element of homeEditLogout) {
@@ -144,8 +142,8 @@ for (let element of homeEditLogout) {
   }
 }
 
-//------------------------------------------------------------------------//
-//------------------------- Création de la modale ------------------------//
+//----------------------------------------------------------------------------------//
+//------------------------------ Création de la modale -----------------------------//
 
 let modal = document.getElementById("modal");
 let galleryModal = document.querySelector("#gallery-modal");
@@ -153,46 +151,46 @@ let galleryModal = document.querySelector("#gallery-modal");
 // La modale est fermée par défaut
 let isModalOpen = false;
 
-//---------------- switch modale v1 v2 v3 --------------//
-// Je crée une fonction pour switcher entre la v1, la v2 ou la v3
+//----------------------------------------------------------------------------------//
+//------------------------------- switch modale v1 v2 v3 ---------------------------//
+// Fonction pour switcher entre la v1, la v2 ou la v3
 function switchModal(id) {
   // les trois v sont en "none" par défaut
   document.getElementById("modal-v1").style.display = "none";
   document.getElementById("modal-v2").style.display = "none";
   document.getElementById("modal-v3").style.display = "none";
-  // je ferai appel à la fonction pour les rendre "block"
+  // Appel à la fonction pour les rendre "block"
   document.getElementById(id).style.display = "block";
 }
 
-//------------------------------------------------------------------------//
-//----------------------------- Ouverture Modale -------------------------//
+//----------------------------------------------------------------------------------//
+//--------------------------------- Ouverture Modale -------------------------------//
 
 const openModal = function (e) {
   e.preventDefault();
-  //Je régénère la modale avec une gallery vide pour afficher les éléments mis à jour
+  // Régénère la modale avec une gallery vide pour afficher les éléments mis à jour
   galleryModal.innerHTML = "";
-  //Je parcours le tableau des works pour intégrer chaque work dans des éléments html créés ci-dessous
+  // Parcours le tableau des works pour intégrer chaque work dans des éléments html créés ci-dessous
   for (let jsonWork of jsonWorks) {
-    let work = new Work(jsonWork);
-
     galleryModal.innerHTML += `<figure>
-     <a href="#" class="delete-work" data-id="${work.id}"><i class="fa-solid fa-trash-can"></i></a>
-     <img src=${work.imageUrl}>
+     <a href="#" class="delete-work" data-id="${jsonWork.id}"><i class="fa-solid fa-trash-can"></i></a>
+     <img src=${jsonWork.imageUrl}>
      <figcaption>Éditer</figcaption>
      </figure>`;
   }
 
-  // J'annule le display:none pour faire apparaitre la modale
+  // Annule le display:none pour faire apparaitre la modale
   modal.style.display = null;
 
-  //---------------- Supprimer un work depuis la modale ------------------//
+  //---------------------------------------------------------------------------//
+  //--------------------- Supprimer un work depuis la modale ------------------//
   document.querySelectorAll(".delete-work").forEach(function (trash) {
     trash.addEventListener("click", function (e) {
       e.preventDefault();
-      // Je récupère l'id du work
+      // Récupère l'id du work
       let workId = this.dataset.id;
 
-      // Je fais un appel API delete avec l'id
+      // Appel API delete avec l'id
       fetch(`${urlBase}/works/${workId}`, {
         method: "DELETE",
         headers: {
@@ -200,10 +198,10 @@ const openModal = function (e) {
         },
       })
         .then(async function (response) {
-          // je fais appel aux fonction GetWorks (récup les news travaux de l'API) puis GenererWorks (mise à jour de l'affichage de la galerie)
+          // Appel aux fonction GetWorks (récup les news travaux de l'API) puis GenererWorks (mise à jour de l'affichage de la galerie)
           jsonWorks = await getWorks();
           genererWorks(jsonWorks);
-          // je fais appel à la fonction closeModale après suppression
+          // Appel à la fonction closeModale après suppression
           closeModal();
         })
         .catch((error) => {
@@ -214,23 +212,23 @@ const openModal = function (e) {
         });
     });
   });
-  //Je fais appel à la fonction pour faire apparaitre la v1 dans ce cas
+  // Appel à la fonction pour faire apparaitre la v1 dans ce cas
   switchModal("modal-v1");
   isModalOpen = true;
 };
 
-//La modale s'ouvre en cliquant sur le lien "modifier"
+// La modale s'ouvre en cliquant sur le lien "modifier"
 document.querySelector(".js-modal").addEventListener("click", openModal);
 
-//---------------------------------------------------------------------------------//
-//----------------------------- Fermeture de la modale ----------------------------//
+//-------------------------------------------------------------------------------------//
+//-------------------------------- Fermeture de la modale -----------------------------//
 const closeModal = function (e) {
   if (e) {
     e.preventDefault();
   }
-  // Je remets la modale en display:none
+  // Remets la modale en display:none
   modal.style.display = "none";
-  // J'efface la photo uploadée, le titre, la selection categorie et les messages d'erreur si closeModal
+  // Efface la photo uploadée, le titre, la selection categorie et les messages d'erreur si closeModal
   removePhoto();
   removeTitle();
   removeCategory();
@@ -239,29 +237,29 @@ const closeModal = function (e) {
   isModalOpen = false;
 };
 
-// J'évite la fermeture de la modale juste en cliquant sur la modale
-// Elle se fermera uniquement si je clique sur la croix ou en cliquant à l'exterieur de la modale
+// Évite la fermeture de la modale juste en cliquant sur la modale
+// Se fermera uniquement si click sur la croix ou click à l'exterieur de la modale
 const stopPropagation = function (e) {
   e.stopPropagation();
 };
 
-//La modale se ferme en cliquant sur la croix v1, v2 ou v3
+// Modale se ferme en cliquant sur la croix v1, v2 ou v3
 modal.querySelectorAll(".js-modal-close").forEach(function (btn) {
   btn.addEventListener("click", closeModal);
 });
 modal.addEventListener("click", closeModal);
 
-//La modale ne se fermera qu'en cliquant à l'exterieur de la modale ou sur la croix
+// Modale ne se fermera qu'en cliquant à l'exterieur de la modale ou sur la croix
 modal
   .querySelector(".modal-wrapper")
   .addEventListener("click", stopPropagation);
 
-//Je switche sur la v2 en cliquant sur le bouton "Ajout photo"
+// Switche sur la v2 en cliquant sur le bouton "Ajout photo"
 document.querySelector(".btn-ajout").addEventListener("click", function () {
   switchModal("modal-v2");
 });
 
-//Je switche sur la v1 en cliquant sur la flèche retour
+// Switche sur la v1 en cliquant sur la flèche retour
 document
   .querySelector(".js-retour-modal1")
   .addEventListener("click", function () {
@@ -272,35 +270,36 @@ document
     messageErrFormModal.style.visibility = "hidden";
   });
 
-// Je peux fermer la modale en appuyant sur escape
+// Fermer la modale en appuyant sur escape
 window.addEventListener("keydown", function (e) {
   if (e.key === "Escape" || e.key === "Esc") {
     closeModal(e);
   }
 });
 
-//---------------------- Effacer le contenu du formulaire ----------------//
-// Je crée une fonction pour effacer la photo uploadée
+//---------------------------------------------------------------------------------//
+//--------------------------- Effacer le contenu du formulaire --------------------//
+// Fonction effacer la photo uploadée
 function removePhoto() {
   imageForm.value = null;
   document.querySelector("#display-image").style.backgroundImage = null;
   document.querySelector(".display-image-none").style.display = "block";
 }
 
-// Je crée une fonction pour effacer le titre
+// Fonction effacer le titre
 const titleForm = document.querySelector("#title");
 function removeTitle() {
   titleForm.value = null;
 }
 
-// Je crée une fonction pour effacer la categorie selectionnée
+// Fonction effacer la categorie selectionnée
 const categoryForm = document.querySelector("#category");
 function removeCategory() {
   categoryForm.value = null;
 }
 
-//-----------------------------------------------------------------------//
-//-------------------- Upload photo dans le formulaire ------------------//
+//---------------------------------------------------------------------------------//
+//------------------------ Upload photo dans le formulaire ------------------------//
 const imageForm = document.querySelector("#image");
 var uploadedImage = "";
 
@@ -316,10 +315,10 @@ imageForm.addEventListener("change", function () {
   reader.readAsDataURL(this.files[0]);
 });
 
-//------------------------------------------------------------------------//
-//-------------- Création liste déroulante formulaire modale -------------//
+//--------------------------------------------------------------------------------//
+//------------------ Création liste déroulante formulaire modale -----------------//
 
-// Je crée les <options> de la balise <select> en parcourant la liste des catégories de l'API
+// Crée les <options> de la balise <select> en parcourant la liste des catégories de l'API
 
 for (let i = 0; i < jsonCategories.length; i++) {
   const selectForm = document.getElementById("category");
@@ -333,10 +332,10 @@ for (let i = 0; i < jsonCategories.length; i++) {
   selectForm.appendChild(optionForm);
 }
 
-//--------------------------------------------------------------------------//
-//---------------- Changement couleur bouton submit ------------------------//
+//--------------------------------------------------------------------------------//
+//---------------------- Changement couleur bouton submit ------------------------//
 
-// Le bouton Valider passera de gris à vert si tous les champs sont remplis
+// Bouton Valider passera de gris à vert si tous les champs sont remplis
 function submitFormColor() {
   if (
     imageForm.value.length > 0 &&
@@ -356,7 +355,7 @@ categoryForm.addEventListener("change", submitFormColor);
 //--------------------------------------------------------------------------------//
 //-------------------- Envoi des données du formulaire modale --------------------//
 
-//Je récupère les données du formulaire
+// Récupère les données du formulaire
 const projectForm = document.querySelector(".modal-form");
 let messageErrFormModal = document.querySelector(".erreur-form-modal");
 
@@ -368,7 +367,7 @@ projectForm.addEventListener("submit", function (e) {
   const title = formData.get("title");
   const category = formData.get("category");
 
-  // Je crée une instruction if pour contrôler la validité de tous les champs
+  // Instruction if pour contrôler la validité de tous les champs
   if (image.size == 0) {
     messageErrFormModal.innerText = "Veuillez mettre une image";
     messageErrFormModal.style.visibility = "visible";
@@ -395,7 +394,7 @@ projectForm.addEventListener("submit", function (e) {
     return 0;
   }
 
-  // J'envoie les données du nouveau projet en POST via fetch
+  // Envoie les données du nouveau projet en POST via fetch
   fetch(`${urlBase}/works`, {
     method: "POST",
     headers: {
@@ -408,7 +407,7 @@ projectForm.addEventListener("submit", function (e) {
       // GetWorks puis GenererWorks pour mettre à jour les données sur la modale et la gallery
       jsonWorks = await getWorks();
       genererWorks(jsonWorks);
-      // Si validation des données ok, je switch vers la v3 pour message de réussite
+      // Si validation des données ok, switch vers la v3 pour message de réussite
       switchModal("modal-v3");
     })
     .catch((error) => {
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,45 +1,25 @@
-// --------------------------------------------------------------------------------------//
-// Je fais appel à une API de validation pour que les champs soient correctement renseignés
-
-// Je pointe le submit auquel je crée un évenement au click
-document
-  .querySelector('.form input[type="submit"]')
-  .addEventListener("click", function () {
-    var valid = true;
-    // Je parcours tous les inputs
-    for (let input of document.querySelectorAll(".form input")) {
-      // Je crée une instruction if pour contrôler la validité de tous les champs
-      // équivaut à valid = valid && input.reportValidity();
-      valid &= input.reportValidity();
-      // Si champs non valide, la validaton s'arrête et ne poursuit pas sur les autres champs
-      if (!valid) {
-        break;
-      }
-    }
-  });
-
-// -------------------------------------------------------------------------//
-// Je fais un appel fetch pour récupérer le userId et token d'authentification
+// ---------------------------------------------------------------//
+// Appel fetch pour récupérer le userId et token d'authentification
 
 const urlBase = "http://localhost:5678/api";
 const urlLogin = urlBase + "/users/login";
 
-// Je pointe ma classe html .form et ajoute un événement sur le submit
+// Pointe classe html .form et ajoute événement sur le submit
 document
   .querySelector(".form")
   .addEventListener("submit", async function (event) {
     try {
-      // J'annule le comportement par défaut du navigateur (chargement d'une nouvelle page à la validation du formulaire)
+      // Annule le comportement par défaut du navigateur (chargement d'une nouvelle page à la validation du formulaire)
       event.preventDefault();
 
-      // Je récupère les données eventuellement stockées dans le localStorage
+      // Récupère les données eventuellement stockées dans le localStorage
       let dataResponse = window.localStorage.getItem("dataResponse");
 
       let messageErreurLogin = document.querySelector(".erreur-login");
 
       // Si aucune donnée sauvegardée dans le localStorage
       if (dataResponse === null) {
-        // J'envoie les données de connexion et demande à récupérer une reponse via fetch
+        // Envoie les données de connexion et demande à récupérer une reponse via fetch
         const response = await fetch(urlLogin, {
           method: "POST",
           headers: {
@@ -52,20 +32,20 @@ document
         });
         dataResponse = await response.json();
 
-        // Je crée une condition pour rediriger l'utilisateur sur la page home en cas de connexion
+        // Condition pour rediriger l'utilisateur sur la page home en cas de connexion
         if ("token" in dataResponse) {
           document.location.href = "index.html";
 
-          //Je convertis dataResponse en JSON avant de les enregistrer dans localStorage
+          // Convertis dataResponse en JSON avant de les enregistrer dans localStorage
           const valeurData = JSON.stringify(dataResponse);
           // Stockage des informations dans le localStorage
           window.localStorage.setItem("dataResponse", valeurData);
         } else {
-          // Si erreur connexion, je renvoie un msg d'erreur
+          // Si erreur connexion, renvoie un msg d'erreur
           messageErreurLogin.style.visibility = "visible";
         }
       } else {
-        //Si les données sont déjà sauvegardées, je les reconstruis en mémoire, afin de rester connecté tant que pas déloguée
+        //Si données déjà sauvegardées, les reconstruis en mémoire, afin de rester connecté tant que pas délogué
         dataResponse = JSON.parse(dataResponse);
       }
     } catch (error) {
@@ -73,7 +53,7 @@ document
     }
   });
 
-// je recupère les data et je les parse
+// Récupère les data et les parse
 let dataResponse = window.localStorage.getItem("dataResponse");
 dataResponse = JSON.parse(dataResponse);
 // pour rediriger automatiquement vers la page index si logged
